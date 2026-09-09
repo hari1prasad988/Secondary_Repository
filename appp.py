d2635f1d85e514ef823b3276312a0153329e6802
hari1prasad988 <prasadreddytech1988@gmail.com>
2026-09-09 12:24:54 +0530

    Update appp.py

---
+++ appp.py
@@ -0,0 +1,9 @@
+# Create a list of fruits
+fruits = ["apple", "banana", "cherry"]
+
+# Add a new item to the list
+fruits.append("orange")
+
+# Loop through the list and print each item
+for fruit in fruits:
+    print(f"I like to eat {fruit}s")
